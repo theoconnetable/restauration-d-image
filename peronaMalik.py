e5d01aae7f7131eb4b3e8c39e7056b9cdb912811
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2023-05-28 21:19:16 +0200

    perona malik corrigé

--- peronaMalik.py
+++ peronaMalik.py
@@ -5,32 +5,6 @@ def g(x):
     """Fonction de tendance à diminuer sur l'intervalle [0, inf[."""
     return 1 / (1 + x**2)
 
-##def gradient_x(img):
-##    """Calcul de la composante x du gradient de l'image."""
-##    img = np.array(img)
-##    h, w = img.shape[:2]
-##    gx = np.zeros((h, w))
-##    for y in range(h):
-##        gx[y, 0] = img[y, 1] - img[y, 0]
-##        gx[y, w-1] = img[y, w-1] - img[y, w-2]
-##    for x in range(1, w-1):
-##        for y in range(h):
-##            gx[y, x] = (img[y, x+1] - img[y, x-1]) / 2
-##    return gx
-##
-##def gradient_y(img):
-##    """Calcul de la composante y du gradient de l'image."""
-##    h, w = img.shape[:2]
-##    gy = np.zeros((h, w))
-##    for x in range(w):
-##        gy[0, x] = img[1, x] - img[0, x]
-##        gy[h-1, x] = img[h-1, x] - img[h-2, x]
-##    for x in range(w):
-##        for y in range(1, h-1):
-##            gy[y, x] = (img[y+1, x] - img[y-1, x]) / 2
-##    return gy
-
-
 def gradient_x(img):
     img = np.array(img)
     h, w = img.shape[:2]
@@ -59,31 +33,11 @@ def test(img):
             part1 = g(abs(gx[y,x])) * gx[y,x] - g(abs(gx[y,x])) * gx[y,x-1]
             part2 = g(abs(gx[y,x])) * gx[y,x] - g(abs(gx[y,x])) * gx[y-1,x]
             div[y,x] = part1 + part2
-            ##div[y,x] = g(abs(gx[y,x])) * gx[y,x] * ( x + 1/2) - g(abs(gx[y,x])) * gx[y,x] * ( x - 1/2)+ g(abs(gy[y,x])) * gy[y,x] * (y + 1/2) - g(abs(gy[y,x])) * gy[y,x] * (y - 1/2)
-            
+            ##div[y,x] = g(abs(gx[y,x])) * gx[y,x] * ( x + 1/2) - g(abs(gx[y,x])) * gx[y,x] * ( x - 1/2)+ g(abs(gy[y,x])) * gy[y,x] * (y + 1/2) - g(abs(gy[y,x])) * gy[y,x] * (y - 1/2)    
     return div ## +1 pas+1/2 ni -1
 ##Pas à rajouter les 1/2 car val dejà dans tab g(x,y) et g(x-1,y)
 
 
-
-##def divergence(img):
-##    """Calcul de la divergence du champ de vecteurs formé par le gradient de l'image."""
-##    h, w = img.shape[:2]
-##    gx = gradient_x(img)
-##    gy = gradient_y(img)
-##    div = np.zeros((h, w))
-##    ##print("VALEUR ENTREE" + str(img))
-##    for x in range(1, w-1):
-##        div[0, x] = -g(abs(gx[0, x])) * gx[0, x]
-##        div[h-1, x] = -g(abs(gx[h-1, x])) * gx[h-1, x]
-##    for y in range(1, h-1):
-##        div[y, 0] = -g(abs(gy[y, 0])) * gy[y, 0]
-##        div[y, w-1] = -g(abs(gy[y, w-1])) * gy[y, w-1]
-##    for x in range(1, w-1):
-##        for y in range(1, h-1):
-##            div[y, x] = -g(abs(gx[y, x])) * gx[y, x] - g(abs(gy[y, x])) * gy[y, x]
-##    ##print("VALEUR RETOUR" + str(div))
-##    return div
 
 def anisotropic_diffusion(img, dt, num_iter):
     """Application de la diffusion anisotropique sur l'image."""
