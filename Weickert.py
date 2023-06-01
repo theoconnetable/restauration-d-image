d254d47c02f2c53f4c3b3bace43b8b421a232537
AndrienMont <92160718+AndrienMont@users.noreply.github.com>
2023-06-01 17:29:15 +0200

    Weickert

--- Weickert.py
+++ Weickert.py
@@ -1,61 +1,77 @@
 import cv2
 import numpy as np
 
-def g(n):
-    return 1 / (1 + n)
+def weickert_diffusion(image, num_iterations, kappa):
+    # Convertir l'image en niveaux de gris
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def weickert_diffusivity(dux, duy, epsilon=0.001):
-    norm_grad = np.sqrt(np.square(dux) + np.square(duy))
-    c = 1 / np.sqrt(epsilon**2 + norm_grad**2)
-    return c
+    # Initialiser les matrices de diffusion
+    Ix2 = np.zeros_like(gray, dtype=np.float32)
+    Iy2 = np.zeros_like(gray, dtype=np.float32)
+    Ixy = np.zeros_like(gray, dtype=np.float32)
 
-def calc_divergence(u, g, epsilon=0.001):
-    dux, duy = np.gradient(u, edge_order=2)
+    # Calculer les dérivées de l'image
+    sobelx = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=3)
 
-    diffusivity = weickert_diffusivity(dux, duy, epsilon)
+    # Calculer les composantes du tenseur de diffusion
+    Ix2 = sobelx * sobelx
+    Iy2 = sobely * sobely
+    Ixy = sobelx * sobely
 
-    div_x = g(np.abs(dux)) * diffusivity * dux
-    div_y = g(np.abs(duy)) * diffusivity * duy
+    # Appliquer la gaussienne au tenseur de diffusion
+    Ix2 = cv2.GaussianBlur(Ix2, (3, 3), 0)
+    Iy2 = cv2.GaussianBlur(Iy2, (3, 3), 0)
+    Ixy = cv2.GaussianBlur(Ixy, (3, 3), 0)
 
-    divergence = np.zeros_like(u)
-    divergence[:, :-1] += div_x[:, :-1]
-    divergence[:, 1:] -= div_x[:, :-1]
-    divergence[:-1, :] += div_y[:-1, :]
-    divergence[1:, :] -= div_y[:-1, :]
+    for _ in range(num_iterations):
+        # Calculer les valeurs propres du tenseur de diffusion
+        lambda1 = 0.5 * (Ix2 + Iy2 + np.sqrt((Ix2 - Iy2) ** 2 + 4 * Ixy * Ixy))
+        lambda2 = 0.5 * (Ix2 + Iy2 - np.sqrt((Ix2 - Iy2) ** 2 + 4 * Ixy * Ixy))
 
-    return divergence
+        # Calculer les coefficients de diffusion
+        c1 = np.exp(-((lambda1 - kappa) ** 2) / (2 * kappa ** 2))
+        c2 = np.exp(-((lambda2 - kappa) ** 2) / (2 * kappa ** 2))
 
-def anisotropic_diffusion_weickert(u, n_iter, dt, epsilon=0.001):
-    for _ in range(n_iter):
-        divergence = calc_divergence(u, g, epsilon)
-        u += dt * u * np.power(divergence, 2)
+        # Diffuser l'image selon les valeurs propres du tenseur de diffusion
+        diffused = c1 * sobelx + c2 * sobely
 
-    return u
+        # Mettre à jour les dérivées de l'image avec la diffusion
+        sobelx = diffused * sobelx
+        sobely = diffused * sobely
 
-# Charger l'image en niveaux de gris
-img = cv2.imread('paysage.jpg', cv2.IMREAD_GRAYSCALE)
+    # Reconstruire l'image couleur en utilisant les dérivées diffusées
+    filtered = cv2.merge((sobelx.astype(np.uint8), sobely.astype(np.uint8), sobely.astype(np.uint8)))
 
-# Normaliser l'image
-img_normalized = img.astype(np.float32) / 255.0
 
-# Appliquer la diffusion anisotrope avec Weickert
-n_iter = 10
-dt = 0.1
-epsilon = 0.001
-result = anisotropic_diffusion_weickert(img_normalized, n_iter, dt, epsilon)
+    # Ajouter l'image originale et l'image filtrée pour obtenir le résultat final
+    result = cv2.addWeighted(image, 0.5, filtered, 0.5, 0)
 
-# Reconvertir l'image normalisée en niveaux de gris
-result_scaled = (result * 255).astype(np.uint8)
+    return result
+
+
+# Charger l'image
+image = cv2.imread("paysage.jpg")
+
+# Paramètres de la diffusion anisotropique
+num_iterations = 5  # Nombre d'itérations de diffusion
+kappa = 0.2  # Paramètre de régularisation
+
+# Appliquer la diffusion anisotropique
+result = weickert_diffusion(image, num_iterations, kappa)
 
 scale_percent = 10 # percent of original size
-width = int(result_scaled.shape[1] * scale_percent / 100)
-height = int(result_scaled.shape[0] * scale_percent / 100)
+width = int(result.shape[1] * scale_percent / 100)
+height = int(result.shape[0] * scale_percent / 100)
 dim = (width, height)
-  
-# resize image
-resized = cv2.resize(result_scaled, dim, interpolation = cv2.INTER_AREA)
 
-# Afficher l'image résultante
-cv2.imshow('Diffusion anisotrope avec Weickert', resized)
+# resize image
+res = cv2.resize(result, dim, interpolation = cv2.INTER_AREA)
+img = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+concat  = np.concatenate((img, res), axis=1)
+
+# Afficher l'image originale et le résultat
+cv2.imshow("Diffusion par Weickert", concat)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
